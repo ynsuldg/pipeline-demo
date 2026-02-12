d07680c47a9df75626a1a0ae2be6e07e9f73c61f
ynsuldg <yns.uldg@hotmail.com>
2026-02-12 10:43:34 +0100

    initial commit - pipeline-demo

---
+++ src/main/java/se/iths/yunus/pipelinedemo/PipelineDemoApplication.java
@@ -0,0 +1,13 @@
+package se.iths.yunus.pipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipelineDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/yunus/pipelinedemo/PipelineDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.yunus.pipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
